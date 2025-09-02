050ee84ecbc7782d49a919a346d2e943d405eeae
Mohmad&W <42297664+Hamodea@users.noreply.github.com>
2025-09-02 11:29:11 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Vad heter du?");
+        String name = Console.ReadLine();
+        Console.WriteLine("Hur gamal är du?");
+        int age = Console.ReadLine();
+        Console.WriteLine(name, age);
+    }
+}
