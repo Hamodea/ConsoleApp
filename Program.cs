0a9a76d02d49217c94b55dca641609f8f8730111
Mohmad&W <42297664+Hamodea@users.noreply.github.com>
2025-09-04 11:23:57 +0200

    new uppdate

--- Program.cs
+++ Program.cs
@@ -1,10 +1,84 @@
 ﻿internal class Program
-{
+{   
+
+
+    static Random rmg = new Random();
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("***************************");
+        Console.WriteLine("Welcome to Hang Man Game..!");
+        Console.WriteLine("***************************");
+        
+        
+        startGame();
+
          // Adding a Comment
 
         // Adding  anthoer comment
+    }
+
+ 
+    // Start Game Method
+    static void startGame()
+    {
+        List<string> wordList = new List<string>() { "computer", "programming", "hangman", "challenge", "developer" };
+        string selectedWord = wordList[rmg.Next(wordList.Count)];
+        int nbrofGuesses = 6;
+        Console.WriteLine("Let's Start the Game!");
+        Console.WriteLine("You have 6 tries to guess the Word! ");
+        Console.WriteLine("Good Luck!");
+        Console.WriteLine("The word has " + selectedWord.Length + " letters.");
+        Console.WriteLine("You can start guessing now.");
+
+        // Displaying underscores for the word
+        char[] guessedLetters = new char[selectedWord.Length];
+        for (int i = 0; i < guessedLetters.Length; i++)
+        {
+            guessedLetters[i] = '_';
+        }
+        Console.WriteLine(string.Join(" ", guessedLetters));
+
+        // Game Loop
+        while (nbrofGuesses > 0)
+        {
+            Console.Write("Enter a letter: ");
+            char guessedLetter = Console.ReadLine()[0];
+            bool isCorrectGuess = false;
+            if (guessedLetters == null || guessedLetters.Length == 0)
+            {
+                Console.WriteLine("invaild input. Tru again");
+
+            }
+            for (int i = 0; i < selectedWord.Length; i++)
+            {
+                if (selectedWord[i] == guessedLetter)
+                {
+                    guessedLetters[i] = guessedLetter;
+                    isCorrectGuess = true;
+                }
+            }
+            if (!isCorrectGuess)
+            {
+                nbrofGuesses--;
+                Console.WriteLine("Wrong guess! You have " + nbrofGuesses + " tries left.");
+            }
+            else
+            {
+                Console.WriteLine("Good guess!");
+            }
+            Console.WriteLine(string.Join(" ", guessedLetters));
+            if (!guessedLetters.Contains('_'))
+            {
+                Console.WriteLine("Congratulations! You've guessed the word: " + selectedWord);
+                return;
+            }
+        }
+
+
+
+
+
+
+        
     }
 }
